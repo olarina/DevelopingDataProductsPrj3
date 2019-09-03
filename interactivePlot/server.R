9201b8b9c34f240f6725a800aef0e7192c724554
olarina <gar_o@bk.ru>
2019-09-03 15:22:37 +0200

    links added

--- interactivePlot/server.R
+++ interactivePlot/server.R
@@ -11,16 +11,18 @@ library(shiny)
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
-   
-  output$distPlot <- renderPlot({
     
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2] 
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-    
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    
-  })
-  
+    output$distPlot <- renderPlot({
+        
+        # generate bins based on input$bins from ui.R
+        x    <- faithful[, 2] 
+        bins <- seq(min(x), max(x), length.out = input$bins + 1)
+        
+        # draw the histogram with the specified number of bins
+        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    })
+    url <- a("gh-pages", href="https://olarina.github.io/DevelopingDataProductsPrj3/projectDescr.html")
+    output$dlink <- renderUI({
+        tagList("More information is available in presentation on", url)
+    })
 })
--- interactivePlot/ui.R
+++ interactivePlot/ui.R
@@ -11,23 +11,25 @@ library(shiny)
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
-  
-  # Application title
-  titlePanel("Old Faithful Geyser Data"),
-  
-  # Sidebar with a slider input for number of bins 
-  sidebarLayout(
-    sidebarPanel(
-       sliderInput("bins",
-                   "Number of bins:",
-                   min = 1,
-                   max = 50,
-                   value = 30)
-    ),
     
-    # Show a plot of the generated distribution
-    mainPanel(
-       plotOutput("distPlot")
+    # Application title
+    titlePanel("Old Faithful Geyser Data"),
+    
+    # Sidebar with a slider input for number of bins 
+    sidebarLayout(
+        sidebarPanel(
+            sliderInput("bins",
+                        "Number of bins:",
+                        min = 1,
+                        max = 50,
+                        value = 30)
+        ),
+        
+        # Show a plot of the generated distribution
+        mainPanel(
+            tabsetPanel(type = "tabs",
+                        tabPanel("Interactive plot", br(), plotOutput("distPlot")),
+                        tabPanel("Documentation", br(),"Write a shiny application with associated supporting documentation. The documentation should be thought of as whatever a user will need to get started using your application.Write a shiny application with associated supporting documentation. The documentation should be thought of as whatever a user will need to get started using your application.", br(), uiOutput("dlink")))
+        )
     )
-  )
 ))
